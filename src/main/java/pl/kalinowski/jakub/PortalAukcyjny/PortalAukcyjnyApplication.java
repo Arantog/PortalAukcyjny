cfc24c73234d87137cf0a71900cc6903a19c05ca
Arantog <arantog@gmail.com>
2021-03-06 10:12:24 +0100

    initialize

---
+++ src/main/java/pl/kalinowski/jakub/PortalAukcyjny/PortalAukcyjnyApplication.java
@@ -0,0 +1,13 @@
+package pl.kalinowski.jakub.PortalAukcyjny;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortalAukcyjnyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortalAukcyjnyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pl/kalinowski/jakub/PortalAukcyjny/PortalAukcyjnyApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.kalinowski.jakub.PortalAukcyjny;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortalAukcyjnyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
